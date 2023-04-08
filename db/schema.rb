b567441fdaa36749ca33e367b5623c112e08cc16
evShun <124552406+evShun@users.noreply.github.com>
2023-04-08 23:56:31 +0900

    bag fixed

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2023_04_04_105638) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nicknake", default: "", null: false
+    t.string "nickname", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
