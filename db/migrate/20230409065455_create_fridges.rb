5e3e32ebc9f6dbd276d657b57a090fd9dbf852e0
evShun <124552406+evShun@users.noreply.github.com>
2023-04-12 10:50:31 +0900

    view

---
+++ db/migrate/20230409065455_create_fridges.rb
@@ -0,0 +1,10 @@
+class CreateFridges < ActiveRecord::Migration[6.0]
+  def change
+    create_table :fridges do |t|
+      t.string     :name ,null: false
+      t.references :user ,null: false, foreign_key: true
+      
+      t.timestamps
+    end
+  end
+end
