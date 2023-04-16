4fa666d74bf2f7a6dfe26f56b822e39e4cc47850
evShun <124552406+evShun@users.noreply.github.com>
2023-04-17 03:12:00 +0900

    some changes

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,18 +1,41 @@
 class FoodsController < ApplicationController
   # before_action :set_food , only: [:show, :create]
+  require 'openai'
+  before_action :set_common_variable, only: [:search]
+
   def show
-    @food = Food.new    
+    @food = Food.new
     @foods = Food.where(fridge_id: params[:id])
-    # binding.pry
   end
-  def create
+
+  def update
     @food = Food.new(food_params)
+    @foods = Food.where(fridge_id: params[:id])
     if @food.save
-      redirect_to fridge_food_path(id: params[:fridge_id])
-      # redirect_to fridge_food_path(@food.fridge_id) 
+      redirect_to fridge_food_path(fridge_id:current_user.id ,id:params[:id])
     else
       render "show"
     end
+  end
+
+  def destroy
+    food = Food.find(params[:id])
+  end
+ 
+  def search
+    @foods = Food.where(fridge_id: params[:id])
+
+    content = "以下の食材を使用して作成できる料理を教えてください
+              #{@foods} "
+    binding.pry
+    response = @client.chat(
+               parameters: {
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: content}], # Required.
+                temperature: 0.7,
+                stream: true,
+                           })
+    @recipe = response.dig("choices", 0, "message", "content")
   end
 
   private
@@ -21,7 +44,11 @@ class FoodsController < ApplicationController
   end
 
   def food_params
-    params.require(:food).permit(:item).merge(fridge_id: params[:fridge_id])
+    params.require(:food).permit(:item).merge(fridge_id: params[:id])
+  end
+
+  # APIキーを設定
+  def set_common_variable
+    @client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
   end
 end
-  
